d391299619f81b9395305271caa4e1a63a8aa37d
Sagar141005 <samsaini5566@gmail.com>
2025-08-28 14:22:17 +0530

    Bug Fix

--- src/app/api/invite/[inviteId]/route.ts
+++ src/app/api/invite/[inviteId]/route.ts
@@ -1,16 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(
-  req: Request,
+  req: NextRequest,
   context: { params: { inviteId: string } }
 ) {
   const { inviteId } = context.params;
   const { action } = await req.json(); // "ACCEPT" or "REJECT"
 
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession({ req, ...authOptions });
   if (!session?.user?.id) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
