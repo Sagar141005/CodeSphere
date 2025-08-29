4e2f57ec59fc3b2945944b59686fb8dfc60b3df4
Sagar141005 <samsaini5566@gmail.com>
2025-08-29 12:15:57 +0530

    Backend URL fix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,9 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
+const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
+  "https://my-socket-server-5tzd.onrender.com";
 
 export const getSocket = (): Socket => {
   if (!socket) {
